42850b8cf2636c3d352d948c32d91aee42b74ba9
kabir02091999 <kabir.02091999@gmail.com>
2023-02-19 11:51:09 -0400

    cambios

--- src/routes/Cliente/PedidosCliente.jsx
+++ src/routes/Cliente/PedidosCliente.jsx
@@ -1,33 +1,56 @@
-import { css } from '@emotion/react'
+/* import { css } from '@emotion/react' */
 import React from 'react'
 import {  Button, Typography } from '@mui/material';
-import '../../styles/styles.css';
+/* import '../../styles/styles.css'; */
 import LayoutSuperior from '../../components/molecules/LayoutSuperiorPedidos';
+
+import './css/card.css'
+import imagen from './css/cine.jpeg'
+
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
 function PedidosCliente() {
 
       
   return (
 
-    <div
-        css={css`
-        display: grid;
-        place-items: center;
-            align-items:center;
-            height:100vh;
-            text-align: center;
-        `}>
+    <div>
+      <div className='view'>
+        <LayoutSuperior nombre="Pedidos" /> 
+        <div className='main'>
+          <div className="container">
+            
+            <div className="card1">
+              <div className="div1">
+                <div className="ima">
+                  <img src={imagen}></img>
+                </div>
+                {/* <div className="tipotike">
+                  <h4>tickes</h4>
+                  <p>Bs. 100</p>
+                </div> */}
+                {/* <div className="metodo">
+                  <h4>metodo de pago</h4>
+                  <p>efectivo</p>
 
-          <h2>pedidos</h2>
+                </div> */}
 
-    <div className='view'
-        >
-          <LayoutSuperior nombre="Pedidos" />
-          <div className='main'> 
-              
+              </div>
+              <div className="div2">
+                <h4>No factura</h4>
+                <p>KFF5TZS</p>
+                <h4>1 febrero, 2023</h4>
+                <p>01:41 pm</p>
+                <div className="a1">
+                  <a href="#">Ver Mas</a>
+                </div>
+              </div>
+            </div>
           </div>
-
-    </div></div>
+        </div>
+      </div>
+    </div>
   )
   }
 
-export default PedidosCliente
+export default PedidosCliente;
