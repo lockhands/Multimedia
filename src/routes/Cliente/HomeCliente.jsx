79e7b4263396c1c90d2d2423f652e801178fb825
kabir02091999 <kabir.02091999@gmail.com>
2023-02-21 01:21:19 -0400

    cambios

--- src/routes/Cliente/HomeCliente.jsx
+++ src/routes/Cliente/HomeCliente.jsx
@@ -1,8 +1,10 @@
-import { css } from '@emotion/react'
 import React from 'react'
+import { css } from '@emotion/react'
 import {Paper, Box, Card, Typography,CardContent,CardActions,Button,Grid} from '@mui/material';
 import '../../styles/styles.css';
-
+import '../../styles/card.css'
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 const bull = (
     <Box
       component="span"
@@ -77,61 +79,8 @@ function HomeCliente (){
 </div>
    
 </div>
+
 </div>
-  /* </Grid> */
-   
-
-    
-
-        </div>
-          <div className="div2">
-            <h4>No factura</h4>
-            <p>KFF5TZS</p>
-            <h4>1 febrero, 2023</h4>
-            <p>01:41 pm</p>
-            <div className="a1">
-              <a href="#">Ver Mas</a>
-            </div>
-          </div>
-        </div>
-
-
-
-
-        <div className="card1">
-          <div className="div1">
-            <div className="icono">
-              <ShoppingCartIcon className="icono1" /* sx={{ color: '#FF8308',height: '50px',width: '50px',position: 'absolute', left: '45px',top: '35px', }} */  />
-            </div>
-            <div className="tipotike">
-
-              <h4>pedidos</h4>
-              <p>Bs. 100</p>
-
-            </div>
-            <div className="metodo">
-
-              <h4>metodo de pago</h4>
-              <p>efectivo</p>
-
-            </div>
-
-          </div>
-          <div className="div2">
-            <h4>No factura</h4>
-            <p>AUEQK35S</p>
-            <h4>2 febrero, 2023</h4>
-            <p>05:41 pm</p>
-            <div className="a1">
-              <a href="#">Ver Mas</a>
-            </div>
-          </div>
-        </div>  
-
-      </div>
-
-      
-      </div>
     );
 }
 
--- src/routes/Cliente/PedidosCliente.jsx
+++ src/routes/Cliente/PedidosCliente.jsx
@@ -4,7 +4,7 @@ import {  Button, Typography } from '@mui/material';
 /* import '../../styles/styles.css'; */
 import LayoutSuperior from '../../components/molecules/LayoutSuperiorPedidos';
 
-import './css/card.css'
+import './css/card1.css'
 import imagen from './css/cine.jpeg'
 
 import AssignmentIcon from '@mui/icons-material/Assignment';
@@ -18,34 +18,56 @@ function PedidosCliente() {
       <div className='view'>
         <LayoutSuperior nombre="Pedidos" /> 
         <div className='main'>
-          <div className="container">
+          <div className="container1">
             
             <div className="card1">
               <div className="div1">
                 <div className="ima">
                   <img src={imagen}></img>
                 </div>
-                {/* <div className="tipotike">
-                  <h4>tickes</h4>
-                  <p>Bs. 100</p>
-                </div> */}
-                {/* <div className="metodo">
-                  <h4>metodo de pago</h4>
-                  <p>efectivo</p>
-
-                </div> */}
-
               </div>
               <div className="div2">
                 <h4>No factura</h4>
-                <p>KFF5TZS</p>
-                <h4>1 febrero, 2023</h4>
-                <p>01:41 pm</p>
+                <p>cripetas grandes 230 gr bebidad grandes 798ml</p>
+                
                 <div className="a1">
-                  <a href="#">Ver Mas</a>
+                  <a href="#">cantidad:  05</a>
                 </div>
               </div>
             </div>
+            
+            <div className="card2">
+              <div className="div1">
+                <div className="ima">
+                  <img src={imagen}></img>
+                </div>
+              </div>
+              <div className="div2">
+                <h4>No factura</h4>
+                <p>cripetas grandes 230 gr bebidad grandes 798ml</p>
+                
+                <div className="a1">
+                  <a href="#">cantidad:  05</a>
+                </div>
+              </div>
+            </div>
+            <div className="card2">
+              <div className="div1">
+                <div className="ima">
+                  <img src={imagen}></img>
+                </div>
+              </div>
+              <div className="div2">
+                <h4>No factura</h4>
+                <p>cripetas grandes 230 gr bebidad grandes 798ml</p>
+                
+                <div className="a1">
+                  <a href="#">cantidad:  05</a>
+                </div>
+              </div>
+            </div>
+            
+
           </div>
         </div>
       </div>
